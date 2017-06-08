592ccadf9e6d605c756fe0830e62947b2539adec
ThibHlln <thibault.hallouin@ucdconnect.ie>
2017-06-08 18:57:12 +0100

    documented simuMain
    also changed hard coded intro for questions asked to user in console

--- simuMain.py
+++ simuMain.py
@@ -9,13 +9,27 @@ import simuFunctions as sFn
 
 def main():
 
-    # User-defined parameters
-    catchment = "Morell"
-    outlet = "IE_EA_09M010300"
+    # Ask user for information on simulation
+    question_catch = raw_input('Name of the catchment? ')
+    catchment = question_catch.capitalize()
 
-    my_simu_start = (2007, 1, 1, 9, 0, 0)
-    my_simu_end = (2007, 1, 10, 9, 0, 0)
-    time_step_in_minutes = 1440.0
+    question_catch = raw_input('European Code (EU_CD) of the catchment? ')
+    outlet = question_catch.upper()
+
+    question_start = raw_input('Starting date for simulation? [format DD/MM/YYYY HH:MM:SS] ')
+    try:
+        datetime_start = datetime.datetime.strptime(question_start, '%d/%m/%Y %H:%M:%S')
+    except ValueError:
+        sys.exit("The starting date format entered is incorrect [not compliant with DD/MM/YYYY HH:MM:SS].")
+
+    question_end = raw_input('Ending date for simulation? [format DD/MM/YYYY HH:MM:SS] ')
+    try:
+        datetime_end = datetime.datetime.strptime(question_end, '%d/%m/%Y %H:%M:%S')
+    except ValueError:
+        sys.exit("The ending date format entered is incorrect [not compliant with DD/MM/YYYY HH:MM:SS].")
+
+    question_time_step = raw_input('Time step for simulation? [in minutes] ')
+    time_step_in_minutes = float(question_time_step)
 
     # Location of the different needed folders
     root = os.path.realpath('..')  # move to parent directory of this current python file
@@ -35,28 +49,25 @@ def main():
     handler.setLevel(logging.WARNING)
     logger.addHandler(handler)
 
-    # Convert all time information in datetime format, and create a TimeFrame object
-    datetime_start = datetime.datetime(my_simu_start[0], my_simu_start[1], my_simu_start[2],
-                                       my_simu_start[3], my_simu_start[4], my_simu_start[5])
-    datetime_end = datetime.datetime(my_simu_end[0], my_simu_end[1], my_simu_end[2],
-                                     my_simu_end[3], my_simu_end[4], my_simu_end[5])
-    time_steps = TimeFrame(datetime_start, datetime_end, int(time_step_in_minutes)).get_list_datetime()
+    # Create a TimeFrame object
+    my__time_frame = TimeFrame(datetime_start, datetime_end, int(time_step_in_minutes))
+    time_steps = my__time_frame.get_list_datetime()
 
     # Declare all the dictionaries that will be needed, all using the waterbody code as a key
     dict__models = dict()  # key: waterbody, value: list of model objects
     dict_meteo = dict()  # key: waterbody, value: data frame (x: time step, y: meteo data type)
-    dict_storage = dict()  # key: waterbody, value: data frame (x: time step, y: data type)
+    dict__data_frames = dict()  # key: waterbody, value: data frame (x: time step, y: data type)
 
     # Create a Network object from network and waterBodies files
     my__network = Network(catchment, outlet, input_folder)
 
-    # Initiate everything needed for the nodes
+    # Create DataFrames for the nodes
     my_variables = ["q_h2o", "c_no3", "c_nh4", "c_dph", "c_pph", "c_sed"]
     for node in my__network.nodes:
-        dict_storage[node] = DataFrame(index=time_steps, columns=my_variables).fillna(0.0)  # filled with zeros
+        dict__data_frames[node] = DataFrame(index=time_steps, columns=my_variables).fillna(0.0)  # filled with zeros
 
-    # Initiate everything needed for the links (Model objects, DataFrame objects)
-    # And collect meteorological data for the links (Meteo dictionary)
+    # Create Models for links
+    # Collect meteorological data for the links
     for link in my__network.links:
         # Declare Model objects and get meteo DataFrame
         if my__network.categories[link] == "11":  # river headwater
@@ -74,13 +85,13 @@ def main():
         else:  # unknown (e.g. 21 would be a lake headwater)
             sys.exit("Waterbody {}: {} is not a registered type of waterbody.".format(link,
                                                                                       my__network.categories[link]))
-        # Declare DataFrame objects
+        # Create DataFrames for the links
         my_headers = list()
         for model in dict__models[link]:
             my_headers += model.input_names + model.state_names + model.output_names
-        dict_storage[link] = DataFrame(index=time_steps, columns=my_headers).fillna(0.0)  # filled with zeros
+        dict__data_frames[link] = DataFrame(index=time_steps, columns=my_headers).fillna(0.0)  # filled with zeros
 
-    # Read the parameters, or read the descriptors file and generate the parameters
+    # Read the parameters file, or read the descriptors file, generate the parameters, and generate the parameters file
     if os.path.isfile('{}{}_{}.smart.parameters'.format(input_folder, catchment, outlet)):
         dict_param = sF.get_dict_parameters_from_file(catchment, outlet, my__network, dict__models, input_folder)
         dict_desc = sF.get_dict_floats_from_file("descriptors", catchment, outlet, my__network, input_folder)
@@ -92,20 +103,20 @@ def main():
     else:
         sys.exit("SMART parameters are not available from files.")
 
-    # Read in loadings file
+    # Read the annual loadings file and the application files to distribute the loadings for each time step
     dict_annual_loads = sF.get_dict_floats_from_file("loadings", catchment, outlet, my__network, input_folder)
     dict_applications = sF.get_dict_strings_from_file("applications", catchment, outlet, my__network, input_folder)
     dict_loadings = sFn.distribute_loadings_across_year(my__network, dict_annual_loads, dict_applications,
                                                         time_steps, specifications_folder)
 
-    # Read the constants in .const file
+    # Read the constants files
     dict_const = dict()
     dict_const["INCAL"] = sF.get_dict_constants_from_file("INCAL", specifications_folder)
 
-    # Initial for reservoirs
+    # Set the initial conditions
     # # TO BE DEFINED
 
-    # Run the simulation
+    # Simulate
     my_dict_variables = dict()
     for variable in my_variables:
         my_dict_variables[variable] = 0.0
@@ -114,43 +125,46 @@ def main():
         # Calculate runoff and concentrations for each link
         for link in my__network.links:
             for model in dict__models[link]:
-                model.run(my__network, link, dict_storage,
+                model.run(my__network, link, dict__data_frames,
                           dict_desc, dict_param, dict_const, dict_meteo, dict_loadings,
                           step, time_step_in_minutes,
                           logger)
         # Sum up everything coming from upstream for each node
         for node in my__network.nodes:
             if my__network.additions.get(node):  # ignore the node up for headwaters
+                # Sum up the flows
                 for variable in ["q_h2o"]:
                     for link in my__network.additions[node]:
                         if my__network.categories[link] == "11":
-                            my_dict_variables[variable] += dict_storage[link].loc[step, "c_out_" + variable]
+                            my_dict_variables[variable] += dict__data_frames[link].loc[step, "c_out_" + variable]
                         elif my__network.categories[link] == "10":
-                            my_dict_variables[variable] += dict_storage[link].loc[step, "r_out_" + variable]
-                            my_dict_variables[variable] += dict_storage[link].loc[step, "c_out_" + variable]
+                            my_dict_variables[variable] += dict__data_frames[link].loc[step, "r_out_" + variable]
+                            my_dict_variables[variable] += dict__data_frames[link].loc[step, "c_out_" + variable]
                         elif my__network.categories[link] == "20":
-                            my_dict_variables[variable] += dict_storage[link].loc[step, "l_out_" + variable]
-                    dict_storage[node].set_value(step, variable,
-                                                 my_dict_variables[variable])
+                            my_dict_variables[variable] += dict__data_frames[link].loc[step, "l_out_" + variable]
+                    dict__data_frames[node].set_value(step, variable,
+                                                      my_dict_variables[variable])
                     my_dict_variables[variable] = 0.0
+                # Sum up the contaminants
                 for variable in ["c_no3", "c_nh4", "c_dph", "c_pph", "c_sed"]:
                     for link in my__network.additions[node]:
                         if my__network.categories[link] == "11":
-                            my_dict_variables[variable] += dict_storage[link].loc[step, "c_out_" + variable] * \
-                                dict_storage[link].loc[step, "c_out_q_h2o"]
+                            my_dict_variables[variable] += dict__data_frames[link].loc[step, "c_out_" + variable] * \
+                                dict__data_frames[link].loc[step, "c_out_q_h2o"]
                         elif my__network.categories[link] == "10":
-                            my_dict_variables[variable] += dict_storage[link].loc[step, "r_out_" + variable] * \
-                                dict_storage[link].loc[step, "r_out_q_h2o"]
-                            my_dict_variables[variable] += dict_storage[link].loc[step, "c_out_" + variable] * \
-                                dict_storage[link].loc[step, "c_out_q_h2o"]
+                            my_dict_variables[variable] += dict__data_frames[link].loc[step, "r_out_" + variable] * \
+                                dict__data_frames[link].loc[step, "r_out_q_h2o"]
+                            my_dict_variables[variable] += dict__data_frames[link].loc[step, "c_out_" + variable] * \
+                                dict__data_frames[link].loc[step, "c_out_q_h2o"]
                         elif my__network.categories[link] == "20":
-                            my_dict_variables[variable] += dict_storage[link].loc[step, "l_out_" + variable] * \
-                                dict_storage[link].loc[step, "l_out_q_h2o"]
+                            my_dict_variables[variable] += dict__data_frames[link].loc[step, "l_out_" + variable] * \
+                                dict__data_frames[link].loc[step, "l_out_q_h2o"]
                     if my_dict_variables["q_h2o"] > 0.0:
-                        dict_storage[node].set_value(step, variable,
-                                                     my_dict_variables[variable] / my_dict_variables["q_h2o"])
+                        dict__data_frames[node].set_value(step, variable,
+                                                          my_dict_variables[variable] / my_dict_variables["q_h2o"])
                     my_dict_variables[variable] = 0.0
 
+    # Save the DataFrames for the links (separating inputs, states, and outputs)
     for link in my__network.links:
         my_inputs = list()
         my_states = list()
@@ -159,15 +173,17 @@ def main():
             my_inputs += model.input_names
             my_states += model.state_names
             my_outputs += model.output_names
-        dict_storage[link].to_csv('{}{}_{}.inputs'.format(output_folder, catchment.capitalize(), link),
-                                  columns=my_inputs, float_format='%e')
-        dict_storage[link].to_csv('{}{}_{}.states'.format(output_folder, catchment.capitalize(), link),
-                                  columns=my_states, float_format='%e')
-        dict_storage[link].to_csv('{}{}_{}.outputs'.format(output_folder, catchment.capitalize(), link),
-                                  columns=my_outputs, float_format='%e')
+        dict__data_frames[link].to_csv('{}{}_{}.inputs'.format(output_folder, catchment.capitalize(), link),
+                                       columns=my_inputs, float_format='%e')
+        dict__data_frames[link].to_csv('{}{}_{}.states'.format(output_folder, catchment.capitalize(), link),
+                                       columns=my_states, float_format='%e')
+        dict__data_frames[link].to_csv('{}{}_{}.outputs'.format(output_folder, catchment.capitalize(), link),
+                                       columns=my_outputs, float_format='%e')
+
+    # Save the DataFrames for the nodes
     for node in my__network.nodes:
-        dict_storage[node].to_csv('{}{}_{}.node'.format(output_folder, catchment.capitalize(), node),
-                                  float_format='%e')
+        dict__data_frames[node].to_csv('{}{}_{}.node'.format(output_folder, catchment.capitalize(), node),
+                                       float_format='%e')
 
 
 if __name__ == "__main__":
