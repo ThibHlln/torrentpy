c1b1e752bf24b899a54015a4d045f224ef217dfb
thibhlln <thibault.hallouin@ucdconnect.ie>
2017-07-31 22:35:30 +0100

    documented code and docstrings (unfinished)

--- simuClasses.py
+++ simuClasses.py
@@ -10,23 +10,42 @@ import simuFunctions as sFn
 
 
 class Network:
-    """"""
+    """
+    This class defines all the constituting parts of a catchment structures as a node-link network, as well as the
+    different relationships between the nodes and the links, and the characteristics of the links.
+    """
     def __init__(self, catchment, outlet, input_folder, specs_folder):
+        # name of the catchment
         self.name = catchment.capitalize()
+        # european code of the outlet
         self.code = outlet.upper()
+        # path of the files necessary to generate the Network object
         self.networkFile = "{}{}_{}.network".format(input_folder, catchment, outlet)
         self.waterBodiesFile = "{}{}_{}.waterbodies".format(input_folder, catchment, outlet)
         self.descriptorsFile = "{}{}_{}.descriptors".format(input_folder, catchment, outlet)
+        # list of the nodes contained in the Network
         self.nodes = Network.get_network_attributes(self)["nodes"]
+        # list of the links contained in the Network
         self.links = Network.get_network_attributes(self)["links"]
+        # connections for links = nodes upstream and downstream of a given link
         self.connections = Network.get_network_attributes(self)["connections"]
+        # adding for nodes = basin surrounding the link that has the given node upstream
         self.adding = Network.get_network_attributes(self)["adding"]
+        # routing for nodes = list of the streams upstream that pour into the given node
         self.routing = Network.get_network_attributes(self)["routing"]
+        # categories for links = code to identify the type of catchment (river or lake, headwater or not)
         self.categories = Network.get_waterbodies_attributes(self)
+        # descriptors for the links = physical descriptors characteristic of a given catchment
         self.descriptors = Network.get_catchments_descriptors(self)
+        # list of the variables to be propagated through the node-link network
         self.variables = Network.get_one_list(specs_folder, "variables")
 
     def get_network_attributes(self):
+        """
+        This method reads all the information contained in the network file in order to get the list of the nodes and
+        the links as well as the connections, and the links adding to the nodes and routed by the nodes.
+        :return: dictionary containing nodes, links, connections, routing, and adding
+        """
         try:
             with open(self.networkFile) as my_file:
                 my_reader = csv.DictReader(my_file)
@@ -60,6 +79,11 @@ class Network:
         }
 
     def get_waterbodies_attributes(self):
+        """
+        This method reads the attributes of the links from the waterBodies file. It associates the waterbody type code
+        (1 for river, 2 for lake) and the headwater status (1 if headwater, 0 if not) to create a 2-digit code.
+        :return: dictionary {key: link, value: category as a 2-digit code}
+        """
         try:
             with open(self.waterBodiesFile) as my_file:
                 my_reader = csv.DictReader(my_file)
@@ -73,6 +97,10 @@ class Network:
         return my_categories
 
     def get_catchments_descriptors(self):
+        """
+        This method reads the catchment descriptors from the descriptors file.
+        :return: nested dictionary {key: link, value: {key: descriptor_name, value: descriptor_value}}
+        """
         try:
             with open(self.descriptorsFile) as my_file:
                 my_descriptors = dict()
@@ -98,6 +126,13 @@ class Network:
 
     @staticmethod
     def get_one_list(specs_folder, specs_type):
+        """
+        This method reads the list of variables for the specification type given as a parameter from the file of
+        specifications in the specification folder.
+        :param specs_folder: path to the specification folder where to find the specifications file
+        :param specs_type: name of the specification type
+        :return: a list of the names of the variables for the given specification
+        """
         my_list = list()
         try:
             with open(specs_folder + "SIMULATOR.spec") as my_file:
@@ -122,19 +157,38 @@ class Network:
 
 
 class Model:
-    """"""
+    """
+    This class defines a model associated to a link in the Network object. It contains the names of the inputs, states,
+    parameters, and outputs as well as the values of the parameters and the constants.
+    """
     def __init__(self, category, identifier, my__network, link, specs_folder, input_folder, output_folder):
+        # category of the Model (catchment, river, or lake)
         self.category = category.upper()
+        # name of the Model within the category
         self.identifier = identifier.upper()
+        # european code of the link associated to the Model
         self.link = link.upper()
+        # list of the names for the inputs of the Model
         self.input_names = Model.get_list_names(self, specs_folder, "inputs")
+        # list of the names for the parameters of the Model
         self.parameter_names = Model.get_list_names(self, specs_folder, "parameters")
+        # list of the names for the states of the Model
         self.state_names = Model.get_list_names(self, specs_folder, "states")
+        # list of the names for the outputs of the Model
         self.output_names = Model.get_list_names(self, specs_folder, "outputs")
+        # values for the parameters of the Model
         self.parameters = Model.get_dict_parameters(self, my__network, specs_folder, input_folder, output_folder)
+        # values for the constants of the Model
         self.constants = Model.get_dict_constants(self, specs_folder)
 
     def get_list_names(self, specs_folder, specs_type):
+        """
+        This method reads the list of variables names for the specification type given as a parameter from the file of
+        specifications in the specification folder.
+        :param specs_folder: path to the specification folder where to find the specifications file
+        :param specs_type: name of the specification type
+        :return: a list of the names of the variables for the given specification
+        """
         components = self.identifier.split('_')
         my_list = list()
         for component in components:
@@ -163,6 +217,15 @@ class Model:
         return my_list
 
     def get_dict_constants(self, specs_folder):
+        """
+        This method get the list of the names for the constants of the Model in its specification file in the
+        specifications folder. Then, the methods reads the constants values in the constant file for the Model located
+        in the specifications folder.
+
+        :param specs_folder: path to the specification folder where to find the specifications file
+        :return: dictionary containing the constants names and values
+            {key: constant_name, value: constant_value}
+        """
         specs_type = "constants"
         components = self.identifier.split('_')
         my_list = list()
@@ -206,6 +269,20 @@ class Model:
         return my_dict
 
     def get_dict_parameters(self, network, specs_folder, input_folder, output_folder):
+        """
+        This method get the list of the names for the parameters of the Model in its specification file in the
+        specifications folder. Then, the methods reads the parameters values in the parameter file for the Model located
+        in the input folder. If this file does not exist, it uses the descriptors of the catchment that owns the Model
+        in order to infer the parameters from the descriptors. In any case, it saves the parameters used in a file in
+        the output folder.
+
+        :param network: Network object for the simulated catchment
+        :param specs_folder: path to the specification folder where to find the specifications file
+        :param input_folder: path to the specification folder where to find the parameter file
+        :param output_folder: path to the specification folder where to save the parameter file
+        :return: dictionary containing the constants names and values
+            {key: parameter_name, value: parameter_value}
+        """
         specs_type = "parameters"
         components = self.identifier.split('_')
         my_list = list()
@@ -269,28 +346,51 @@ class Model:
 
         return my_dict
 
-    def run(self, obj_network, waterbody, dict_data_frame,
-            dict_meteo, dict_loads,
+    def run(self, obj_network, waterbody, dict_nd_data,
+            dict_nd_meteo, dict_nd_loadings,
             datetime_time_step, time_gap, logger):
+        """
+        This method runs the Model simulations for one time step.
+
+        :param obj_network: Network object for the simulated catchment
+        :param waterbody: code of the link that owns the model
+        :param dict_nd_data: dictionary containing the nested dictionaries for the nodes and the links for variables
+            { key = link/node: value = nested_dictionary(index=datetime,column=variable) }
+        :type dict_nd_data: dict
+        :param dict_nd_meteo: dictionary containing the nested dictionaries for the links for meteorological inputs
+            { key = link: value = nested_dictionary(index=datetime,column=meteo_input) }
+        :type dict_nd_meteo: dict
+        :param dict_nd_loadings: dictionary containing the nested dictionaries for the links for contaminant inputs
+            { key = link: value = nested_dictionary(index=datetime,column=contaminant_input) }
+        :param datetime_time_step:
+        :param time_gap: duration of the gap between two simulation time steps
+        :param time_gap: float
+        :param logger: reference to the logger to be used
+        :type logger: Logger
+        :return: NOTHING
+        """
         if self.category == "CATCHMENT":
-            sM.catchment_model(self.identifier, waterbody, dict_data_frame,
-                               obj_network.descriptors, self.parameters, self.constants, dict_meteo, dict_loads,
+            sM.catchment_model(self.identifier, waterbody, dict_nd_data,
+                               obj_network.descriptors, self.parameters, self.constants,
+                               dict_nd_meteo, dict_nd_loadings,
                                datetime_time_step, time_gap,
                                logger)
         elif self.category == "RIVER":
-            sM.river_model(self.identifier, obj_network, waterbody, dict_data_frame,
-                           self.parameters, self.constants, dict_meteo,
+            sM.river_model(self.identifier, obj_network, waterbody, dict_nd_data,
+                           self.parameters, self.constants, dict_nd_meteo,
                            datetime_time_step, time_gap,
                            logger)
         elif self.category == "LAKE":
-            sM.lake_model(self.identifier, waterbody, dict_data_frame,
-                          obj_network.descriptors, self.parameters, dict_meteo,
+            sM.lake_model(self.identifier, waterbody, dict_nd_data,
+                          obj_network.descriptors, self.parameters, dict_nd_meteo,
                           datetime_time_step, time_gap,
                           logger)
 
 
 class TimeFrame:
-    """"""
+    """
+
+    """
     def __init__(self, datetime_start, datetime_end,
                  data_increment_in_minutes, simu_increment_in_minutes,
                  expected_simu_slice_length):
