a6ec22c5ab3f3f9821b48075097250f21a32ebb4
ThibHlln <thibault.hallouin@ucdconnect.ie>
2018-03-08 19:05:05 +0000

    makes postprocMulti more verbose
    postprocMulti was only sending back to console "No handlers found..." so
    that the exception raised was not written anywhere.
    
    so I replicated what was done in simuRunMulti and create a .log file for
    postproc that will log the exception raised

--- postproc/postprocMulti.py
+++ postproc/postprocMulti.py
@@ -1,5 +1,5 @@
 import logging
-from multiprocessing import Pool
+from multiprocessing import Pool, log_to_stderr
 from os import path
 from csv import DictReader
 
@@ -8,34 +8,42 @@ import postprocPerf as ppPf
 import postprocPlot as ppPl
 
 
-def get_arguments_from_batch_file(batch_file, root):
+def get_arguments_from_batch_file(batch_file, root, log_file):
     try:
         with open(batch_file, 'rb') as my_file:
             my_reader = DictReader(my_file)
             my_list_of_tuples = list()
             for line in my_reader:
                 my_list_of_tuples.append((line['Catchment'], line['Outlet'], line['Gauge'],
-                                          root))
+                                          root, log_file))
 
         return my_list_of_tuples
     except IOError:
         raise Exception('{} does not exist.'.format(batch_file))
 
 
-def single_run(catchment, outlet, gauge, root):
+def single_run(catchment, outlet, gauge, root, log_file):
     """
     :param catchment: name of the catchment
     :param outlet: european code of the outlet of the catchment
     :param gauge: identification number of the hydrometric station
     :param root: location of the root of the CSF folder
+    :param log_file: location of the log file to use for the FileHandler
     :return: NOTHING
     """
+    mp_logger = log_to_stderr()
+    mp_logger.setLevel(logging.INFO)
+    handler = logging.FileHandler(log_file)
+    handler.setFormatter(logging.Formatter(fmt='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                                           datefmt="%d/%m/%Y - %H:%M:%S"))
     logger = logging.getLogger("MultiPostProc")
-    logger.setLevel(logging.ERROR)
+    logger.addHandler(handler)
     try:
         ppPl.main(catchment, outlet, gauge, root)
         ppPf.main(catchment, outlet, gauge, root)
     except Exception as e:
+        mp_logger.error('Exception for arguments ({}, {}, {})'.format(catchment, outlet, gauge))
+        logger.error('Exception for arguments ({}, {}, {})'.format(catchment, outlet, gauge))
         logger.exception(e)
         raise e
 
@@ -55,11 +63,12 @@ if __name__ == '__main__':
     input_dir = ''.join([csf_root, "/in/"])
     output_dir = ''.join([csf_root, "/out/"])
 
+    my_log_file = '{}/postprocessing.batch.log'.format(output_dir)
     my_batch_file = '{}/postprocessing.batch'.format(input_dir)
 
     pool = Pool(processes=1, maxtasksperchild=1)
 
-    arguments = get_arguments_from_batch_file(my_batch_file, csf_root)
+    arguments = get_arguments_from_batch_file(my_batch_file, csf_root, my_log_file)
 
     results = pool.imap_unordered(single_run_star, iterable=arguments)
 
