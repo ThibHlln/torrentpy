17b77c9b424163ab08e5c71a8717a42957347788
ThibHlln <thibault.hallouin@ucdconnect.ie>
2018-07-05 19:29:11 +0100

    deletes build/dist added by mistake

--- build/lib/torrentpy/__init__.py
+++
@@ -1,7 +0,0 @@
-from .network import Network
-from .knowledgebase import KnowledgeBase
-from .database import DataBase
-from .timeframe import TimeFrame
-from .batch import Batch
-
-from .utils import connectivity
